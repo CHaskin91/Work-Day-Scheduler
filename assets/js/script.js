9673962afdeac03a621ae01c4531a01481d01c0a
Chubbs91 <chaskin1991@outlook.com>
2021-12-21 23:30:26 -0600

    date/time added - now displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 // jQuery calling <p> ID for currentDay (Jumbotron HTML)
 $("#currentDay").text(date);
+
+// Create a function to show/display the date as text
+function update() {
+    var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    $("#currentDay").text(date);
+};
+
+// Call the Update Function
+update();
+// setInterval to update every second for timer to run continuously
+setInterval(update, 1000);
