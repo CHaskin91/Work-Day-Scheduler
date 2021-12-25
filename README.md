72b425bca1f6d73ca5804ba61dd2ee36c91959b8
Chubbs91 <chaskin1991@outlook.com>
2021-12-25 14:34:55 -0600

    ready to push live.  Need to add past/present/future colors

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ A calendar app for scheduling your work day.
 ## Resources
 * moment.js [https://momentjs.com/]
 * Font Awesome [https://fontawesome.com/v5.15/icons/save?style=regular]
+* Past, Present, Future coloring help [https://stackoverflow.com/questions/59995703/trying-to-change-background-color-based-off-if-the-hour-is-in-the-past-current]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// variable for saving tasks/plans
+var saveBtn = $(".saveBtn");
 // Jumbotron variable for date.  including month/day/year/hour/minutes/seconds
 var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 // jQuery calling <p> ID for currentDay (Jumbotron HTML)
@@ -14,3 +16,42 @@ update();
 // setInterval to update every second for timer to run continuously
 setInterval(update, 1000);
 
+// Create function to color-code timeblocks dependent on past, present, future
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // Save to localStorage
+    localStorage.setItem(time, plan);
+});
+
+function usePlanner() {
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    })
+}
+
+timeBlockColor();
+usePlanner();
